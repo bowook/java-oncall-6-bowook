20ed61ee678a56bd170168582fa46a90b3a732d1
bowook <mbosx316@naver.com>
2024-12-06 21:16:42 +0900

    feat(Holiday) : 휴일 도메인 추가

--- src/main/java/oncall/domain/constant/Holiday.java
+++ src/main/java/oncall/domain/constant/Holiday.java
@@ -1,2 +1,29 @@
-package oncall.domain.constant;public enum Holiday {
+package oncall.domain.constant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum Holiday {
+    JANUARY(1, 0),
+    MARCH(1, 0),
+    MAY(5, 0),
+    JUNE(6, 0),
+    AUGUST(15, 0),
+    OCTOBER(3, 9),
+    DECEMBER(25, 0),
+    NONE(0, 0);
+    private final int dateOne;
+    private final int dateTwo;
+
+    Holiday(final int dateOne, final int dateTwo) {
+        this.dateOne = dateOne;
+        this.dateTwo = dateTwo;
+    }
+
+    public List<Integer> getDate() {
+        List<Integer> dates = new ArrayList<>();
+        dates.add(dateOne);
+        dates.add(dateTwo);
+        return dates;
+    }
 }
