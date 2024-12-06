7bceb4cbbe065abe78f2b42865ad29c1ee70e2a1
bowook <mbosx316@naver.com>
2024-12-06 21:15:43 +0900

    feat(Worker) : 근무자 도메인 추가

--- src/main/java/oncall/domain/Worker.java
+++ src/main/java/oncall/domain/Worker.java
@@ -1,2 +1,13 @@
-package oncall.domain;public class Worker {
+package oncall.domain;
+
+public class Worker {
+    private final String name;
+
+    public Worker(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
