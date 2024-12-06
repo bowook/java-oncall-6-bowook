84056a5c3cfd00c055e3b59e65046628a1d48c10
bowook <mbosx316@naver.com>
2024-12-06 21:16:24 +0900

    feat(Day) : Day Constant 도메인 추가

--- src/main/java/oncall/domain/constant/Day.java
+++ src/main/java/oncall/domain/constant/Day.java
@@ -1,5 +1,37 @@
 package oncall.domain.constant;
 
-public class Date {
-    
+import java.util.Arrays;
+import oncall.exception.CustomException;
+import oncall.exception.ErrorMessage;
+
+public enum Day {
+    MONDAY("월", "평일"),
+    TUESDAY("화", "평일"),
+    WEDNESDAY("수", "평일"),
+    THURSDAY("목", "평일"),
+    FRIDAY("금", "평일"),
+    SATURDAY("토", "주말"),
+    SUNDAY("일", "주말");
+    private final String name;
+    private final String day;
+
+    Day(final String name, final String day) {
+        this.name = name;
+        this.day = day;
+    }
+
+    public static Day from(String input) {
+        return Arrays.stream(Day.values())
+                .filter(day -> day.name.equals(input))
+                .findFirst()
+                .orElseThrow(() -> CustomException.from(ErrorMessage.INVALID_INPUT_ERROR));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDay() {
+        return day;
+    }
 }
