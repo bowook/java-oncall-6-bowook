0be4291581305c29cfabdff70230af43f9f1efc4
bowook <mbosx316@naver.com>
2024-12-06 21:14:55 +0900

    feat(AssignedWorkers) : 할당된 근무자 도메인 추가

--- src/main/java/oncall/domain/AssignedWorkers.java
+++ src/main/java/oncall/domain/AssignedWorkers.java
@@ -1,2 +1,17 @@
-package oncall.domain;public class AssignedWorkers {
+package oncall.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class AssignedWorkers {
+    private final List<Worker> workers = new ArrayList<>();
+
+    public void add(Worker worker) {
+        workers.add(worker);
+    }
+
+    public List<Worker> getWorkers() {
+        return Collections.unmodifiableList(workers);
+    }
 }
