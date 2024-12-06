15bb62069e1860962f8192e6b6fcc2deb473cae0
bowook <mbosx316@naver.com>
2024-12-06 21:16:04 +0900

    feat(Workers) : Worker 일급컬렉션 추가

--- src/main/java/oncall/domain/Workers.java
+++ src/main/java/oncall/domain/Workers.java
@@ -1,2 +1,21 @@
-package oncall.domain;public class Workers {
+package oncall.domain;
+
+import java.util.List;
+
+public class Workers {
+    private final List<Worker> weekWorkers;
+    private final List<Worker> weekendWorkers;
+
+    public Workers(List<Worker> weekWorkers, List<Worker> weekendWorkers) {
+        this.weekWorkers = weekWorkers;
+        this.weekendWorkers = weekendWorkers;
+    }
+
+    public List<Worker> getWeekWorkers() {
+        return weekWorkers;
+    }
+
+    public List<Worker> getWeekendWorkers() {
+        return weekendWorkers;
+    }
 }
