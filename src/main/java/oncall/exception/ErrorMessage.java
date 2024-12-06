0665734ca3c8b71db04dd56c8578ddc78aefcc61
bowook <mbosx316@naver.com>
2024-12-06 21:17:43 +0900

    feat(ErrorMessage) : 에러 메시지 기능 추가

--- src/main/java/oncall/exception/ErrorMessage.java
+++ src/main/java/oncall/exception/ErrorMessage.java
@@ -1,2 +1,15 @@
-package oncall.exception;public enum ErrorMessage {
+package oncall.exception;
+
+public enum ErrorMessage {
+    INVALID_INPUT_ERROR("유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
 }
