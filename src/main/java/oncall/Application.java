9f364be9e770b4e4bf89f3e8e9e0e4bc966a1069
bowook <mbosx316@naver.com>
2024-12-06 21:14:05 +0900

    feat(Application) : 메인 추가

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,14 @@
 package oncall;
 
+import oncall.controller.OnCallController;
+import oncall.service.OnCallService;
+import oncall.view.InputView;
+import oncall.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OnCallController onCallController = new OnCallController(new InputView(), new OutputView(),
+                new OnCallService());
+        onCallController.start();
     }
 }
