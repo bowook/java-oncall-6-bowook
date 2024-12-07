ec6da244348daf687eac34d526b241fae51398f4
bowook <mbosx316@naver.com>
2024-12-07 19:58:14 +0900

    refactor(AssignedWorkers) : 섞기

--- src/main/java/oncall/domain/AssignedWorkers.java
+++ src/main/java/oncall/domain/AssignedWorkers.java
@@ -3,15 +3,33 @@ package oncall.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class AssignedWorkers {
     private final List<Worker> workers = new ArrayList<>();
+    private final Random random = new Random();
 
     public void add(Worker worker) {
         workers.add(worker);
     }
 
     public List<Worker> getWorkers() {
+        ensureNoContinuousWorkers();
         return Collections.unmodifiableList(workers);
+    }
+
+    private void ensureNoContinuousWorkers() {
+        while (hasContinuousWorkers()) {
+            Collections.shuffle(workers, random);
+        }
+    }
+
+    private boolean hasContinuousWorkers() {
+        for (int i = 0; i < workers.size() - 1; i++) {
+            if (workers.get(i).getName().equals(workers.get(i + 1).getName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
